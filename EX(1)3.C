90d49c53b9f7636b0149b390c446dbcb124070ac
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-01-03 21:11:44 +0530

    Add files via upload

---
+++ EX(1)3.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int year;
+	clrscr();
+	printf("Enter a year:\n");
+	scanf("%d",&year);
+	if(year%400==0||year%100!=0 && year%4==0)
+		printf("%d is leap year",year);
+	else
+		printf("%d is not a leap year",year);
+	getch();
+}
