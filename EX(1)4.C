dbf5913660fdb4123a33d1cda0f0ae0aa766bd7d
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-01-04 20:24:21 +0530

    Add files via upload

---
+++ EX(1)4.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char c;
+	clrscr();
+	printf("Enter a charcter:\n");
+	scanf("%c",&c);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'&& c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+		printf("%c is vowel",c);
+	else
+		printf("%c is not a vowel",c);
+	getch();
+}
