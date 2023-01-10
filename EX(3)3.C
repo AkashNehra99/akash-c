ca24dca231ffcc1b9d611d84aef21902fda8f36c
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-01-10 20:18:57 +0530

    Add files via upload

---
+++ EX(3)3.C
@@ -0,0 +1,20 @@
+// Program to print triangular star pattern
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j,n;
+    clrscr();
+    printf("Enter how many rows you want:\n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+     for(j=0;j<i;j++)
+     {
+       printf("*");
+     }
+     printf("\n");
+    }
+    getch();
+}
