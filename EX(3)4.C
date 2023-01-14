792088c3c9f57a20db1f7f8ea22ebf09d54a3b0b
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-01-14 21:25:14 +0530

    Add files via upload

---
+++ EX(3)4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,r,s=0;
+    clrscr();
+    printf("Enter the number you want the digits addition of: ");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+      r=n%10;
+      s=s+r;
+      n=n/10;
+    }
+    printf("Addition of the entered digits of a number is %d",s);
+    getch();
+}
