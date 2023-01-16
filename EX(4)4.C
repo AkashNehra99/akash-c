15f08785b3d9c40125b47fe3f69c8796023f82ed
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-01-16 22:09:21 +0530

    Add files via upload

---
+++ EX(4)4.C
@@ -0,0 +1,12 @@
+// WAP using string input and output function(gets and puts)
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char s[10];
+    printf("Enter a string: ");
+    gets(s);
+    printf("your entered string is: ");
+    puts(s);
+    getch();
+}
