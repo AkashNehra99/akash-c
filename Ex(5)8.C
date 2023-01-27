c5f9f09ce5e9e117c3d2a338e985ced7d4088271
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-01-27 20:31:07 +0530

    Create Ex(5)8.C

---
+++ Ex(5)8.C
@@ -0,0 +1,20 @@
+// is_lower function program
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char c;
+    void is_lower(char);
+    printf("Enter a character: ");
+    scanf("%c",&c);
+    is_lower(c);
+    getch();
+}
+void is_lower(char x)
+{
+    if(x>='a'&&x<='z')
+      printf("Character is in lower case");
+    else
+      printf("Character is not in lower case");
+}
