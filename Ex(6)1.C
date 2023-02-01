c7689bd7544e004231513e82c6a86b6012699ed1
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-02-01 20:59:26 +0530

    Create Ex(6)1.C

---
+++ Ex(6)1.C
@@ -0,0 +1,19 @@
+// Factorial(n) using recusrive functions
+
+#include<stdio.h>
+// #include<conio.h>
+void main()
+{
+    int fact(int);
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    printf("Factorial is %d",fact(n));
+}
+int fact(int n)
+{
+    if(n==0)
+      return 1;
+    else 
+      return n*fact(n-1);
+}
