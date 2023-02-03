86d83d8f5fd29103effa58d170a52d41de4b6047
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-02-03 20:40:13 +0530

    Add files via upload

---
+++ EX(6)3.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int m,n;
+    int A(int,int);
+    clrscr();
+    printf("Enter number 1:\n");
+    scanf("%d",&m);
+    printf("Enter number 2:\n");
+    scanf("%d",&n);
+    printf("output: %d",A(m,n));
+    getch();
+}
+int A(int m,int n)
+{
+    if(m==0)
+      return n+1;
+    else if(n==0)
+      return A(m-1,1);
+    else
+      return A(m-1,A(m,n-1));
+}
