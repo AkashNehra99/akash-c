38d1f0b1bb9ff9e5fe87cf76d69f86a0fb6d85a0
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-02-13 22:19:17 +0530

    Add files via upload

---
+++ EX(10)2.C
@@ -0,0 +1,21 @@
+// WAP to get sum of elements of an array using pointers only
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,*b,sum=0;
+    int a[5];
+    b=&a[0];
+    printf("Enter the elements to add: ");
+    for(i=0;i<5;i++)
+    {
+      scanf("%d",b++);
+    }
+    b=&a[0];
+    for(i=0;i<5;i++)
+    {
+      sum+=*(b++);
+    }
+    getch();
+}
