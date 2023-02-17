09d753c3cf950bdbb3af39b7be9270ac08cf517c
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-02-17 10:18:32 +0530

    Add files via upload

---
+++ EX(11)2.C
@@ -0,0 +1,27 @@
+// to get reverse of one string using own string function
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char a[20];
+    void myrevstr(char []);
+    printf("Enter a string: ");
+    gets(a);
+    myrevstr(a);
+    getch();
+}
+void myrevstr(char s[])
+{
+  int l,i,k;
+  char c;
+  l=strlen(s);
+  for(i=0;i<l/2;i++)
+  {
+    k=l-i-1;
+    c=s[k];
+    s[k]=s[i];
+    s[i]=c;
+  }
+  printf("%s",s);
+}
