6909b459fc0e9b9b839543df94dd8e62377ef44a
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-04-26 10:40:50 +0530

    Add files via upload

--- QUEUE.C
+++ QUEUE.C
@@ -10,23 +10,32 @@ void main()
 {
   void enqueue(QUEUE *q,int);
   int dequeue(QUEUE *q);
-  //int isoverflow(QUEUE q);
-  //int isunderflow(QUEUE q);
+  int isoverflow(QUEUE q);
+  int isunderflow(QUEUE q);
   QUEUE q;
   int ch,item;
   clrscr();
   q.front=q.rear=-1;
   while(1)
   {
-    printf("1.Insert element at beginning  2.Delete from first\n");
+    printf("1.Insert element in queue  2.Delete element  3.Exit\n");
     printf("Enter your choice: ");
     scanf("%d",&ch);
     switch(ch)
     {
-     case 1: printf("Enter the element to insert: ");
+     case 1: if(isoverflow(q))
+		printf("Queue is empty\n");
+	     else{
+	     printf("Enter the element to insert: ");
 	     scanf("%d",&item);
 	     enqueue(&q,item);
-     case 2: printf("The dequeued element is %d\n",queue(&q));
+	     }
+	     break;
+     case 2: if(isunderflow(q))
+		printf("Queue is empty\n");
+	     else
+		printf("The dequeued element is %d\n",dequeue(&q));
+	     break;
      case 3: exit(1);
      default: printf("Wrong choice\n");
     }
@@ -36,7 +45,8 @@ void enqueue(QUEUE *q,int item)
 {
   if(q->front==-1)
   {
-   q->front=q->rear=0;
+   q->front=0;
+   q->rear=0;
    q->a[q->rear]=item;
   }
   else if(q->rear<MAX-1)
@@ -66,5 +76,19 @@ int dequeue(QUEUE *q)
   else
     q->front=q->front+1;
   return item;
+}
+int isoverflow(QUEUE q)
+{
+  if(q.a[q.rear]==MAX-1)
+    return 1;
+  else
+    return 0;
+}
+int isunderflow(QUEUE q)
+{
+  if(q.rear==-1)
+    return 1;
+  else
+    return 0;
 }
 
