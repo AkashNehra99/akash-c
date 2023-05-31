c1e08397ebea1a92dabebf081f9186ff1e27adb7
Akash Nehra <117842761+AkashNehra99@users.noreply.github.com>
2023-05-31 10:33:33 +0530

    Add files via upload

--- BS_TREE.C
+++ BS_TREE.C
@@ -7,6 +7,7 @@ typedef struct BST_node
   int item;
   struct BST_node *right;
 }BST;
+
 void insert_BST(BST **tree, int element)
 {
   if(*tree==NULL)
@@ -68,6 +69,49 @@ int totalnode(BST *tree)
   else
     return (totalnode(tree->left) + totalnode(tree->right) +1);
 }
+int determineheight(BST *tree)
+{
+  int lh,rh;
+  if(tree==NULL)
+    return 0;
+  else
+  {
+    lh = determineheight(tree->left);
+    rh = determineheight(tree->left);
+    if(lh>rh)
+      return ++lh;
+    else
+      return ++rh;
+  }
+}
+void deletenode(BST **tree, int element)
+{
+  BST *temp;
+  if(*tree==NULL)
+      printf("Element not found in BST\n");
+  else if(element < (*tree)->item)
+      deletenode(&((*tree)->left),element);
+  else if(element > (*tree)->item)
+      deletenode(&((*tree)->right),element);
+  else if(((*tree)->left) && ((*tree)->right))
+  {
+    temp = findgreatest((*tree)->left);
+    (*tree)->item= temp->item;
+    deletenode(&((*tree)->left),temp->item);
+  }
+  else
+  {
+    temp = *tree;
+    if(((*tree)->left==NULL) && (*tree)->right==NULL)
+	*tree = NULL;
+    else if((*tree)->left!=NULL)
+	*tree = (*tree)->left;
+    else
+	*tree = (*tree)->right;
+    printf("Element %d is deleted\n",temp->item);
+    free(temp);
+  }
+}
 
 void main()
 {
@@ -79,7 +123,8 @@ void main()
   {
     printf("\n1.Insertion  2.Inorder  3. Preorder  4.Postorder  ");
     printf("5.Find smallest element  6.Find greatest element  ");
-    printf("7.Find total number of nodes  8.exit\n");
+    printf("7.Find total number of nodes   8.Find Height of Tree  ");
+    printf("9.Delete Node   10.exit\n");
     printf("Enter your choice: ");
     scanf("%d",&ch);
     switch(ch)
@@ -109,7 +154,13 @@ void main()
 	     break;
      case 7: printf("Total nodes are: %d",totalnode(root));
 	     break;
-     case 8: exit(1);
+     case 8: printf("Height of the tree is %d",determineheight(root));
+	     break;
+     case 9: printf("Enter element to delete: ");
+	     scanf("%d",&element);
+	     deletenode(&root,element);
+	     break;
+     case 10: exit(1);
      default: printf("Invalid choice\n");
      }
     }
